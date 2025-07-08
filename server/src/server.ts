ad6349c4d1380e948e8aeaa9e81616675e0e7818
Nikely Fedechen <fedechenikely@gmail.com>
2025-07-08 13:27:24 -0300

    feat: add config env

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,6 +5,7 @@ import {
   type ZodTypeProvider,
 } from "fastify-type-provider-zod";
 import { fastifyCors } from "@fastify/cors";
+import { env } from "./env.ts";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
@@ -15,6 +16,10 @@ app.register(fastifyCors, {
 app.setSerializerCompiler(serializerCompiler);
 app.setValidatorCompiler(validatorCompiler);
 
-app.listen({ port: 3333 }).then(() => {
+app.get('/health', () => {
+  return 'OK';
+})
+
+app.listen({ port: env.PORT }).then(() => {
   console.log("HTTP server running!");
 });
