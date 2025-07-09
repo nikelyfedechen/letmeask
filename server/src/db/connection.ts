d9e77ce9f0a54d0a465681d75f2f2f9ab916d2ed
Nikely Fedechen <fedechenikely@gmail.com>
2025-07-09 16:56:41 -0300

    feat: configure DB tables with Drizzle

--- server/src/db/connection.ts
+++ server/src/db/connection.ts
@@ -1,8 +1,10 @@
+import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import { env } from '../env.ts';
+import { schema } from './schema/index.ts';
 
 export const sql = postgres(env.DATABASE_URL);
-
-const result = await sql`SELECT 'Hello' as message`;
-
-console.log(result);
+export const db = drizzle(sql, {
+  schema,
+  casing: 'snake_case',
+});
---
+++ server/src/db/schema/index.ts
@@ -0,0 +1,7 @@
+// Barrel File:  um arquivo que reexporta arquivos que estão dentro da pasta.
+
+import { rooms } from './rooms.ts';
+
+export const schema = {
+  rooms,
+};
