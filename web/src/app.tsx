f931c937224a5457fe6ff32accc97a289f22b6a1
Nikely Fedechen <fedechenikely@gmail.com>
2025-07-09 21:20:56 -0300

    feat: define routes

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,5 +1,16 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import CreateRoom from './pages/CreateRoom';
+import Room from './pages/Room';
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<CreateRoom />} index />
+        <Route element={<Room />} path="/room" />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
