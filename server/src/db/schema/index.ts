3b51b0d90e69c4e01d3fb4fe12304e7fb3b19b7a
Nikely Fedechen <fedechenikely@gmail.com>
2025-07-10 21:59:24 -0300

    feat: create questions schema

--- server/src/db/schema/index.ts
+++ server/src/db/schema/index.ts
@@ -1,7 +1,9 @@
 // Barrel File:  um arquivo que reexporta arquivos que estão dentro da pasta.
 
+import { questions } from './questions.ts';
 import { rooms } from './rooms.ts';
 
 export const schema = {
   rooms,
+  questions,
 };
